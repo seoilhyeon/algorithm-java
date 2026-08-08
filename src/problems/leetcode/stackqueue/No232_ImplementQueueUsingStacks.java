18b955906ba1872a23914bbf33aba2f4c7fd94d1
seoilhyeon <ilhyeon0326@gmail.com>
2026-08-09 01:13:12 +0900

    [Leetcode 232] Implement Queue using Stacks

---
+++ src/problems/leetcode/stackqueue/No232_ImplementQueueUsingStacks.java
@@ -0,0 +1,46 @@
+package problems.leetcode.stackqueue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class No232_ImplementQueueUsingStacks {
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.peek();
+     * boolean param_4 = obj.empty();
+     */
+    class MyQueue {
+
+        Deque<Integer> input = new ArrayDeque<>(), output = new ArrayDeque<>();
+
+        public MyQueue() {
+
+        }
+
+        public void push(int x) {
+            input.push(x);
+        }
+
+        public int pop() {
+            peek();
+            return output.pop();
+        }
+
+        public int peek() {
+            if (output.isEmpty()) {
+                while (!input.isEmpty()) {
+                    output.push(input.pop());
+                }
+            }
+            return output.peek();
+        }
+
+        public boolean empty() {
+            return input.isEmpty() && output.isEmpty();
+        }
+    }
+}
