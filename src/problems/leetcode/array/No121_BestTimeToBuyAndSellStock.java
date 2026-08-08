61a0d5b0090b55a5f2925a69baa04babdc7711ca
seoilhyeon <ilhyeon0326@gmail.com>
2026-08-08 11:52:43 +0900

    [Leetcode 121] Best Time to Buy and Sell Stock

---
+++ src/problems/leetcode/array/No121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+package problems.leetcode.array;
+
+public class No121_BestTimeToBuyAndSellStock {
+
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int buy = prices[0], maxProfit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                buy = Math.min(buy, prices[i]);
+                maxProfit = Math.max(maxProfit, prices[i] - buy);
+            }
+            return maxProfit;
+        }
+    }
+}
