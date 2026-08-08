4ee54be4521e2a0360aba74f3f598d73fd38e5a9
seoilhyeon <ilhyeon0326@gmail.com>
2026-08-08 18:48:48 +0900

    [Leetcode 328] Odd Even Linked List

---
+++ src/problems/leetcode/linkedlist/No328_OddEvenLinkedList.java
@@ -0,0 +1,27 @@
+package problems.leetcode.linkedlist;
+
+import types.leetcode.ListNode;
+
+public class No328_OddEvenLinkedList {
+
+    class Solution {
+        public ListNode oddEvenList(ListNode head) {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode oddRoot = new ListNode(0, head), evenRoot = new ListNode(0, head.next);
+            ListNode odd = head, even = head.next;
+
+            while (odd.next != null && odd.next.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenRoot.next;
+
+            return oddRoot.next;
+        }
+    }
+}
